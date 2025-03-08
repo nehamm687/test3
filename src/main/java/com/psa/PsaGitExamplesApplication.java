0713480d607e843dcfbda207d98d4b9a06ed0dc1
nehamm687 <74143408+nehamm687@users.noreply.github.com>
2025-03-08 09:37:05 +0530

    Update PsaGitExamplesApplication.java

--- src/main/java/com/psa/PsaGitExamplesApplication.java
+++ src/main/java/com/psa/PsaGitExamplesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PsaGitExamplesApplication {
-
+	int x=100;
 	public static void main(String[] args) {
 		SpringApplication.run(PsaGitExamplesApplication.class, args);
 	}
